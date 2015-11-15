33ac98e7cdacd4fdd421bb2b5d4d4f5f56e7891e
Leelddd <Leelddd@outlook.com>
2015-11-15 22:50:56 +0800

    1015/11/15 homeworkover:)

---
+++ Chapter10/ex_01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+//int main(){
+//	vector<int> intVec{ 0, 2, 3, 4, 5, 3, 4, 2, 1, 3, 4, 2, 4 };
+//	cout << count(intVec.begin(), intVec.end(), 2);
+//	getchar();
+//}
